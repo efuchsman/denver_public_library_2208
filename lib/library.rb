09caa436b46e85613a02ee7db1c3749e2b8f1177
Eli Fuchsman <efuchsman@gmail.com>
2022-09-14 10:17:52 -0600

    Add method and test for checking library books and authors

--- lib/library.rb
+++ lib/library.rb
@@ -1,10 +1,21 @@
 class Library
 
-  attr_reader :name
+  attr_reader :name, :books, :authors
 
   def initialize(name)
     @name = name
+    @books = []
+    @authors = []
 
   end
+
+  def add_author(author)
+    @author = author
+    @authors << author
+     author.books.each do |book|
+      @books << book
+     end
+  end
+
 
 end
