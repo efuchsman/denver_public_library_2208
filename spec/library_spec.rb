fb5eee66d6b4ef4153f23f68d41b09f8642c6d84
Eli Fuchsman <efuchsman@gmail.com>
2022-09-14 10:05:15 -0600

    Create readable attributes test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe Library do
     dpl = Library.new("Denver Public Library")
     expect(dpl).to be_an_instance_of(Library)
   end
+
+  it "has readable attributes" do
+    dpl = Library.new("Denver Public Library")
+    expect(dpl.name).to eq("Denver Public Library")
+
+  end
+
 end
